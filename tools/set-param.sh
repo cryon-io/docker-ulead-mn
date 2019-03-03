eaee1a509ae164f7418208b03e806210da7c836a
cryi <cryi@tutanota.com>
2019-03-03 15:09:56 +0100

    fix set-param to support ipv6 addresses

--- tools/set-param.sh
+++ tools/set-param.sh
@@ -26,7 +26,14 @@ VALUE=$(echo "$1" | sed "s/[^>]*=//")
 case $PARAM in
     ip)
         TEMP=$(sed "s/externalip=.*/externalip=$VALUE/g" "$BASEDIR/../data/ulead.conf")
-        TEMP=$(echo "$TEMP" | sed "s/masternodeaddr=.*/masternodeaddr=$VALUE:11788/g")
+        case $VALUE in 
+            *:*)
+                TEMP=$(echo "$TEMP" | sed "s/masternodeaddr=.*/masternodeaddr=[$VALUE]:11788/g")
+            ;;
+            *)
+                TEMP=$(echo "$TEMP" | sed "s/masternodeaddr=.*/masternodeaddr=$VALUE:11788/g")
+            ;;
+        esac
         printf "%s" "$TEMP" > "$BASEDIR/../data/ulead.conf"
     ;;
     nodeprivkey)
